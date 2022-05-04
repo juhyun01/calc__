974abd4a561f4849657daee51df5e21fac017e70
JUHYUN LEE <98452653+juhyun01@users.noreply.github.com>
2022-05-04 09:22:30 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup (
+   include_package_data = True,
+   name='calc100',
+   version='0.0.1',
+   description='oss-dev calc100',
+   author='juhyun01',
+   author_email='hean2@donga.ac.kr',
+   url = "https://github.com/juhyun01",
+   install_requires=['pytest'],
+   long_description = 'thisisoss calc python module',
+   long_description_content_type = 'text/markdown',
+   classifiers=[
+       "Programming Language :: Python :: 3",
+       "Operating System :: OS Independent",
+   ],
+)
